d1c3dfdb281a99b29ed552e4e3ef4811ad7e65c7
Alec <Incubusalec@gmail.com>
2015-10-10 22:06:32 +0000

    add 403 page and rescue from cancan forbidden error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from CanCan::AccessDenied do |exception|
+    render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
+  end
+
+  def is_admin?
+	    current_user && current_user.admin?
+	end
+
+  helper_method :is_admin?
+
 end
