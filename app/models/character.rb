81d1026d6614884d2319b038f7164724f646016b
Alec <Incubusalec@gmail.com>
2015-10-13 19:06:49 +0000

    add presence validators for character. Add testing

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,10 @@
 class Character < ActiveRecord::Base
   belongs_to :campaign
   has_one :user, :through => :campaign
+
+  validates :name, presence: true
+  validates :faction, presence: true
+  validates :xp, presence: true
+  validates :credits, presence: true
+
 end
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Character, type: :model do
   # it { should have_one :user }
 
   it { should belong_to :campaign }
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :faction }
+  it { should validate_presence_of :xp }
+  it { should validate_presence_of :credits }
+
 end
