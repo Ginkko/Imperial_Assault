6bc9cceaef1ea2c546a593f16e6efeaeade53ea6
Alec <Incubusalec@gmail.com>
2015-10-13 22:45:28 +0000

    update seeds. Update home#index and user#show styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 users = User.create([
-    { name: "Voltron", email: "Voltron@voltron.com", password: "456456456", password_confirmation: "456456456", admin: true },
-    { name: "Dave", email: "Dave@dave.com", password: "456456456", password_confirmation: "456456456" }
+    { name: "Voltron", email: "Voltron@voltron.com", password: "456456456", password_confirmation: "456456456", current_sign_in_at: DateTime.now, admin: true },
+    { name: "Dave", email: "Dave@dave.com", password: "456456456", password_confirmation: "456456456", current_sign_in_at: DateTime.now },
+    { name: "Ginkko", email: "Ginkko@ginkko.com", password: "456456456", password_confirmation: "456456456", current_sign_in_at: DateTime.now },
+    { name: "Eiliaz", email: "Eiliaz@Eiliaz.com", password: "456456456", password_confirmation: "456456456", current_sign_in_at: DateTime.now },
+    { name: "Cheeseman", email: "Cheeseman@cheeseman.com", password: "456456456", password_confirmation: "456456456", current_sign_in_at: DateTime.now },
+    { name: "Aender", email: "Aender@Aender.com", password: "456456456", password_confirmation: "456456456", current_sign_in_at: DateTime.now }
 ])
+
+campaigns = Campaign.create([
+    { name: "Voltron's Campaign", created_by_id: 1, user_ids: [1,2] },
+    { name: "Dave's Campaign", created_by_id: 2, user_ids: [2,5,6] },
+    { name: "Leauge of Champions", created_by_id: 6, user_ids: [1,3,4,5,6] }
+  ])
